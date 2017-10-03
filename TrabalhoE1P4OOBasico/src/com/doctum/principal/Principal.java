f59af7d8b7a755c2778ec20efa6312d9360b9533
joaovitorg <32442865+joaovitorg@users.noreply.github.com>
2017-10-02 22:53:59 -0300

    Update Principal.java

--- TrabalhoE1P4OOBasico/src/com/doctum/principal/Principal.java
+++ TrabalhoE1P4OOBasico/src/com/doctum/principal/Principal.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.doctum.principal;
 
 import com.doctum.oo.Compra;
